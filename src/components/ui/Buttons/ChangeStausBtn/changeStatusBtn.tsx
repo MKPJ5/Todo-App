ba1678ace35ac46578d5cb5fec42f0a35532a8ed
MKPJ <bmkpj5@gmail.com>
2026-08-27 18:44:54 +0330

    style: add hover cursor pointer to changeStatusBtn

--- src/components/ui/Buttons/ChangeStausBtn/changeStatusBtn.tsx
+++ src/components/ui/Buttons/ChangeStausBtn/changeStatusBtn.tsx
@@ -7,7 +7,7 @@ function ChangeStatusBtn({ taskStatus, onClick }: ChangeStatusBtnProp) {
   return (
     <button
       onClick={() => onClick()}
-      className={`flex h-10 w-10 items-center justify-center rounded-full border-2 transition-all duration-200 hover:scale-110 active:scale-90 ${
+      className={`flex h-10 w-10 items-center justify-center rounded-full border-2 transition-all duration-200 hover:scale-110 hover:cursor-pointer active:scale-90 ${
         taskStatus
           ? "border-green-500 bg-green-500 text-white"
           : "border-green-300 text-green-600 hover:border-green-500 hover:bg-green-50"
