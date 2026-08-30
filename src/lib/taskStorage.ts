e4661a32b12de9b1f5443b855e906d99f44216ad
MKPJ <bmkpj5@gmail.com>
2026-08-30 05:25:30 +0330

    refactor: remove unused functions from taskStorage

--- src/lib/taskStorage.ts
+++ src/lib/taskStorage.ts
@@ -1,40 +1,12 @@
 import type { taskTypes } from "../types/taskTypes";
-interface Task {
-  id: string;
-  title: string;
-  completed: boolean;
-}
 
 const STORAGE_KEY = "tasks";
 
-const getTasks = (): taskTypes[] => {
+export const getTasks = (): taskTypes[] => {
   const data = localStorage.getItem(STORAGE_KEY);
   return data ? JSON.parse(data) : [];
 };
 
-const saveTasks = (tasks: taskTypes[]): void => {
+export const saveTasks = (tasks: taskTypes[]): void => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
 };
-
-const addTask = (task: taskTypes): taskTypes[] => {
-  const tasks = getTasks();
-  const updatedTasks = [...tasks, task];
-  saveTasks(updatedTasks);
-  return updatedTasks;
-};
-
-const deleteTask = (id: string): taskTypes[] => {
-  const tasks = getTasks();
-  const updatedTasks = tasks.filter((t) => t.id !== id);
-  saveTasks(updatedTasks);
-  return updatedTasks;
-};
-
-const updateTask = (updatedTask: taskTypes): taskTypes[] => {
-  const tasks = getTasks();
-  const updatedTasks = tasks.map((t) => (t.id === updatedTask.id ? updatedTask : t));
-  saveTasks(updatedTasks);
-  return updatedTasks;
-};
-
-export { addTask, deleteTask, getTasks, updateTask };
