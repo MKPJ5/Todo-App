465b6ca9bea97e5ae9044a2b44f2183b5de197c9
MKPJ <bmkpj5@gmail.com>
2026-08-30 05:25:49 +0330

    feat: persist tasks with localStorage

--- src/hooks/useTasksSetter.tsx
+++ src/hooks/useTasksSetter.tsx
@@ -1,33 +1,46 @@
+// hooks/useTasksSetter.ts
 import { useState } from "react";
 import type { taskTypes } from "../types/taskTypes";
+import { getTasks, saveTasks } from "../lib/taskStorage";
 
 const useTasksSetter = () => {
-  const [tasks, setTasks] = useState<taskTypes[]>([]);
+  const [tasks, setTasks] = useState<taskTypes[]>(getTasks());
+
   function addTask(task: taskTypes) {
     const newTask = {
       ...task,
       id: crypto.randomUUID(),
     };
-    setTasks((prev) => [...prev, newTask]);
+    setTasks((prev) => {
+      const updatedTasks = [...prev, newTask];
+      saveTasks(updatedTasks);
+      return updatedTasks;
+    });
   }
 
   function toggleStatus(id: string) {
-    setTasks((prev) =>
-      prev.map((task) => (task.id === id ? { ...task, status: !task.status } : task))
-    );
+    setTasks((prev) => {
+      const updatedTasks = prev.map((task) =>
+        task.id === id ? { ...task, status: !task.status } : task
+      );
+      saveTasks(updatedTasks);
+      return updatedTasks;
+    });
   }
 
   function updateTask(id: string, editedTask: taskTypes) {
-    setTasks((prev) =>
-      prev.map((task) =>
+    setTasks((prev) => {
+      const updatedTasks = prev.map((task) =>
         task.id === id
           ? {
               ...editedTask,
               date: `Updated at: ${new Date().toLocaleTimeString()} --- Date: ${editedTask.date}`,
             }
           : task
-      )
-    );
+      );
+      saveTasks(updatedTasks);
+      return updatedTasks;
+    });
   }
 
   return { tasks, addTask, toggleStatus, updateTask };
